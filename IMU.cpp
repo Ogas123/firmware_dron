a213b631732e9422fea6f95079802f9af050390b
Ogas123 <agustinschwerdt@gmail.com>
2026-07-19 15:14:37 -0300

    Calibración LEVENBERG-MARQUARDT

--- IMU.cpp
+++ IMU.cpp
@@ -13,6 +13,22 @@ float offsetRoll = 0, offsetPitch = 0, offsetYaw = 0;
 // Variables para los ángulos brutos del acelerómetro
 float AngleRoll_Acc, AnglePitch_Acc;
 
+// ==========================================================
+// PARÁMETROS CALIBRADOS LEVENBERG-MARQUARDT
+// ==========================================================
+#define ALFA_YX -0.020670
+#define ALFA_ZX 0.012738
+#define ALFA_ZY 0.016160
+
+#define S_X 1.008423
+#define S_Y 0.975793
+#define S_Z 0.956617
+
+#define B_X -0.287793
+#define B_Y -0.028362
+#define B_Z 0.539640
+// ==========================================================
+
 void initIMU() {
   // 1. Iniciar bus I2C en los pines D4(SDA) y D5(SCL) del ESP32-S3
   Wire.begin(PIN_IMU_SDA, PIN_IMU_SCL); 
@@ -69,7 +85,9 @@ void initIMU() {
 
 
 void leerIMU() {
+  // ---------------------------------
   // --- GIROSCOPIO ---
+  // ---------------------------------
   Wire.beginTransmission(0x68);
   Wire.write(0x43); 
   Wire.endTransmission(false); 
@@ -87,7 +105,9 @@ void leerIMU() {
   RateRoll -= offsetRoll;
   RateYaw -= offsetYaw;
 
+  // ---------------------------------
   // --- ACELERÓMETRO ---
+  // ---------------------------------
   Wire.beginTransmission(0x68);
   Wire.write(0x3B); 
   Wire.endTransmission(); 
@@ -97,20 +117,36 @@ void leerIMU() {
   int16_t AccYLSB = Wire.read() << 8 | Wire.read();
   int16_t AccZLSB = Wire.read() << 8 | Wire.read();
 
-  AccX = (float)AccXLSB / 4096.0;
-  AccY = (float)AccYLSB / 4096.0;
-  AccZ = (float)AccZLSB / 4096.0;
+  // 1. Lectura en m/s^2 (multiplicado por 9.80665 para mantener coherencia con Scilab)
+  float AccX_crudo = ((float)AccXLSB / 4096.0) * 9.80665;
+  float AccY_crudo = ((float)AccYLSB / 4096.0) * 9.80665;
+  float AccZ_crudo = ((float)AccZLSB / 4096.0) * 9.80665;
 
-  // 1. Calculamos el ángulo BRUTO con atan2 para evitar la división por cero (Anti-NaN)
+  // 2. Restar el Offset (Vector 'b')
+  float a_x_1 = AccX_crudo - B_X;
+  float a_y_1 = AccY_crudo - B_Y;
+  float a_z_1 = AccZ_crudo - B_Z;
+
+  // 3. Multiplicar por matriz de Escala (Matriz 'K')
+  float a_x_2 = a_x_1 * S_X;
+  float a_y_2 = a_y_1 * S_Y;
+  float a_z_2 = a_z_1 * S_Z;
+
+  // 4. Multiplicar por matriz de Misalignment (Matriz 'T')
+  // Esto sobrescribe las variables globales AccX, AccY, AccZ que usará tu Kalman
+  AccX = a_x_2;
+  AccY = (ALFA_YX * a_x_2) + a_y_2;
+  AccZ = (ALFA_ZX * a_x_2) + (ALFA_ZY * a_y_2) + a_z_2;
+
+  // Opcional: Volver a convertir a unidades de gravedad 'g' si tu Kalman lo prefiere
+  AccX = AccX / 9.80665;
+  AccY = AccY / 9.80665;
+  AccZ = AccZ / 9.80665;
+
+  // 5. Cálculo de ángulos para Kalman (Tu código actual intacto)
   AnglePitch_Acc = atan2(AccY, sqrt(AccX*AccX + AccZ*AccZ)) * RAD_TO_DEG;
   AngleRoll_Acc = -atan2(AccX, sqrt(AccY*AccY + AccZ*AccZ)) * RAD_TO_DEG;
 
-  // 2. LA MAGIA: Pasamos los datos crudos por el Filtro de Kalman
-  // Invocamos la función para Roll (usando la nueva notación)
   kalman(x_hat_Roll, P_Roll, RateRoll, AngleRoll_Acc);
-  
-  // Invocamos la función para Pitch (usando la nueva notación)
   kalman(x_hat_Pitch, P_Pitch, RatePitch, AnglePitch_Acc);
-
-
 }
